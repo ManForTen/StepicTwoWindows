18169147868e456193f5bdcde4d9bbb966c1464c
Artem Petrikov <manforten@tandex.ru>
2023-02-10 22:26:32 +0300

    New changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Два диалоговых окна");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = 500, height = 500;
+        frame.setBounds(dim.width / 2 - width / 2, dim.height / 2 - height / 2, width, height);
+        JPanel panel = new JPanel ();
+        panel.setFocusable(true); // Делаем у панели возможность принимать фокус, иначе она не сможет отловить события клавиатуры
+        frame.add(panel);
+        panel.addKeyListener(new KeyAdapter() {
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_SPACE) // Если нажат пробел
+                {
+                    JOptionPane pane = new JOptionPane(); // Создаем первый диалог
+                    String result = pane.showInputDialog(panel, "Как вас зовут?", "Question", JOptionPane.QUESTION_MESSAGE); // Выводим вопрос
+                    new JOptionPane().showMessageDialog (pane, result,"Ответ", JOptionPane.INFORMATION_MESSAGE); // Выводим ответ
+                }
+            }
+        });
+        frame.setVisible(true);
     }
 }
